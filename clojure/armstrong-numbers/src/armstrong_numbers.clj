1ad8a76680daf9272d0a95e123e46feb33367e0c
pedrao <pedrao@gmail.com>
2019-04-16 15:50:11 -0400

    Armstrong numbers solution

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,10 +1,28 @@
 (ns armstrong-numbers
   (:require [clojure.string :as str]))
 
+(defn num->str [number]
+  (str number))
+
+(defn number-length [number]
+  (count (str number)))
+
+(defn str->bigintvector [strnum]
+  (map biginteger (str/split strnum #"")))
+
+(defn pow-biginteger-vector 
+  [bigintvec exponent]
+  (map (fn [n] (.pow n exponent)) bigintvec))
+
+(defn sum-vector 
+  [powered-vec]
+  (reduce + powered-vec))
+
 (defn armstrong? [number] ;; <- arglist goes here
-  (let [tamanho (count (str number))
-        vetor-str (str/split (str number) #"")
-        vetor-num (map (fn [n] (long (.pow (biginteger n) tamanho))) vetor-str)]
-    (= (reduce + vetor-num) number)
-    )
-  )
+  (let [strnum (num->str number)
+        numlength (number-length strnum)
+        bigint-vec (str->bigintvector strnum)
+        powered-vec (pow-biginteger-vector bigint-vec numlength)
+        numcalculated (sum-vector powered-vec)]
+    (= number numcalculated)
+  ))
